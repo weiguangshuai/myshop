2f8cc20498e70f178e9b80eb9830fa48255c712d
weigs <cqupt_wgs@163.com>
2018-03-22 21:46:41 +0800

    新文件添加内容

--- src/main/java/com/cqupt/project/pay/Demo.java
+++ src/main/java/com/cqupt/project/pay/Demo.java
@@ -5,4 +5,7 @@ package com.cqupt.project.pay;
  * @date 2018/3/22 0022
  */
 public class Demo {
+    public static void main(String[] args) {
+        System.out.println("在weigs分支更改一个文件");
+    }
 }
